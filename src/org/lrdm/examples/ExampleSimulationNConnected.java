c411a5ee610d122aa1560c81743bffef53df10ab
Benjamin-Elias Probst <benjamin-elias.probst@mailbox.tu-dresden.de>
2025-05-13 13:21:40 +0200

    ExampleSimulationNConnected.java

---
+++ src/org/lrdm/examples/ExampleSimulationNConnected.java
@@ -0,0 +1,54 @@
+package org.lrdm.examples;
+
+import org.lrdm.TimedRDMSim;
+import org.lrdm.effectors.Effector;
+import org.lrdm.probes.Probe;
+import org.lrdm.topologies.FullyConnectedTopology;
+import org.lrdm.topologies.NConnectedTopology;
+
+import java.util.List;
+
+/**Simple simulation runner.
+ * 
+ * @author Sebastian Götz <sebastian.goetz1@tu-dresden.de>
+ *
+ */
+public class ExampleSimulationNConnected {
+	public static void main(String[] args) {
+		System.setProperty("java.util.logging.SimpleFormatter.format",
+				"[%1$tF %1$tT] [%4$-7s] %5$s %n");
+		TimedRDMSim sim = new TimedRDMSim();
+		sim.initialize(new NConnectedTopology());
+		Effector effector = sim.getEffector();
+		int mirrors = 8;
+		for(int t = 0; t < 200; t += 20) {
+			effector.setMirrors(mirrors, t);
+			mirrors += 2;
+		}
+		for(int t = 200; t < 400; t += 20) {
+			effector.setMirrors(mirrors, t);
+			mirrors -= 2;
+		}
+		effector.setStrategy(new NConnectedTopology(), 400);
+		for(int t = 500; t < 600; t += 20) {
+			effector.setMirrors(mirrors, t);
+			mirrors += 4;
+		}
+		effector.setStrategy(new FullyConnectedTopology(), 600);
+		for(int t = 700; t < 800; t += 20) {
+			effector.setMirrors(mirrors, t);
+			mirrors -= 1;
+		}
+		effector.setStrategy(new NConnectedTopology(), 800);
+
+		//use this code to manually run the simulation step by step
+		List<Probe> probes = sim.getProbes();
+		int simTime = sim.getSimTime();
+		for (int t = 1; t <= simTime; t++) {
+			for(Probe p : probes) p.print(t);
+
+			sim.runStep(t);
+		}
+		sim.plotLinks();
+	}
+}
