b148052a41af5a362923c5c28f112421784225c1
Benjamin-Elias Probst <benjamin-elias.probst@mailbox.tu-dresden.de>
2025-06-11 19:36:54 +0200

    prepare substructure building

--- src/org/lrdm/topologies/BalancedTreeTopologyStrategy.java
+++ src/org/lrdm/topologies/BalancedTreeTopologyStrategy.java
@@ -196,4 +196,67 @@ public class BalancedTreeTopologyStrategy extends TopologyStrategy, BuildAsSubst
         if(a instanceof MirrorChange mc) m += mc.getNewMirrors();
         return m - 1;
     }
+
+    /**
+     * @param n
+     * @param root
+     * @param mirrorsToConnect
+     * @param props
+     * @return
+     */
+    @Override
+    public Set<Link> initNetworkSub(Network n, Mirror root, List<Mirror> mirrorsToConnect, Properties props) {
+        return Set.of();
+    }
+
+    /**
+     * @param n
+     * @param root
+     * @param props
+     * @param simTime
+     */
+    @Override
+    public void restartNetworkSub(Network n, Mirror root, Properties props, int simTime) {
+
+    }
+
+    /**
+     * @param n
+     * @param newMirrors
+     * @param props
+     * @param simTime
+     */
+    @Override
+    public void handleAddNewMirrorsSub(Network n, int newMirrors, Properties props, int simTime) {
+
+    }
+
+    /**
+     * @param n
+     * @param removeMirrors
+     * @param props
+     * @param simTime
+     */
+    @Override
+    public void handleRemoveMirrorsSub(Network n, int removeMirrors, Properties props, int simTime) {
+
+    }
+
+    /**
+     * @param n
+     * @return
+     */
+    @Override
+    public int getNumTargetLinksSub(Network n) {
+        return 0;
+    }
+
+    /**
+     * @param a
+     * @return
+     */
+    @Override
+    public int getPredictedNumTargetLinksSub(Action a) {
+        return 0;
+    }
 }
